b07ad46eb5cd7b749db06835ba64e7e352d1e613
Inma Mimateach <inmagdiaz92@gmail.com>
2022-07-08 02:57:09 +0200

    first changes made

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# bootcamp_favFilmsBillboard
+# Api películas
+
+Crea con Postman un crud a la Api de películas
+
+## Instalación
+
+- Instala en global el servidor de la Fake Api
+
+    sudo npm install -g json-server
+
+- Ejecuta el json
+
+    json-server --watch peliculas.json
+
+- Instala Postman
+
+    https://www.postman.com/
+
+- Crea un Crud en Postman
+
+- Crea un crud en un frontend.
+
+    Utiliza una carpeta ./public/index.html para sustituir la página de inicio.
+
+- Modifica el Json a tu gusto y añade imágenes de portada de las películas
+
+<a href=https://github.com/typicode/json-server>JSON-Server documentation:</a>
---
+++ js/index.js
@@ -0,0 +1,37 @@
+const requestURL = "./json/peliculas.json";
+
+async function fetchMoviesJSON() {
+const response = await fetch(requestURL);
+const movies = await response.json();
+return movies;
+}
+
+fetchMoviesJSON().then((movies) => {
+for (let index = 0; index < movies.peliculas.length; index++) {
+    const moviesSection = document.getElementById("moviesSection");
+
+    let id = movies.peliculas[index].id;
+    let poster = movies.peliculas[index].image;
+    let title = movies.peliculas[index].nombre;
+    let direccion = movies.peliculas[index].direccion;
+    let genre = movies.peliculas[index].clasificacion;
+    let link = movies.peliculas[index].link;
+
+    moviesSection.innerHTML += `
+        <div class="card" style="width: 20rem;">
+            <img src=${poster} class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${title}</h5>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Dirección: ${direccion}</li>
+                <li class="list-group-item">Género: ${genre}</li>
+            </ul>
+            <div class="card-body">
+                <a href="${link}" class="btn card-link">FilmAffinity</a>
+            </div>
+        </div>
+        `;
+}
+});
+
